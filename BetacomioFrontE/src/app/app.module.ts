994401c8225a9bdb77cee0658c57b14481829368
ChristianvVett <chrisvett8502@gmail.com>
2023-07-13 16:44:45 +0200

    Inizio footer

--- BetacomioFrontE/src/app/app.module.ts
+++ BetacomioFrontE/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './Home/home/home.component';
 import { AboutComponent } from './About/about/about.component';
+import { FooterComponent } from './Footer/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
