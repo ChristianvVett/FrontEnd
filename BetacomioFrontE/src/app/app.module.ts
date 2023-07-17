693d583ae84156dd970ea887cdb6fb20ceea6658
ChristianvVett <chrisvett8502@gmail.com>
2023-07-17 09:31:58 +0200

    Footer completato

--- BetacomioFrontE/src/app/app.module.ts
+++ BetacomioFrontE/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './Home/home/home.component';
 import { AboutComponent } from './About/about/about.component';
+import { NavbarComponent } from './Navbar/navbar/navbar.component';
+import { FooterComponent } from './Footer/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
