53c7e95513fd2a0026a17a392547390006e3d2fe
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-07-18 13:01:22 +0200

    searchbar catalogue

--- BetacomioFrontE/src/app/Catalogue/catalogue/catalogue.component.ts
+++ BetacomioFrontE/src/app/Catalogue/catalogue/catalogue.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import{HttpClient} from '@angular/common/http'
 import {faChevronRight} from '@fortawesome/free-solid-svg-icons'
 import {faChevronLeft} from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-catalogue',
@@ -9,10 +10,17 @@ import {faChevronLeft} from '@fortawesome/free-solid-svg-icons'
   styleUrls: ['./catalogue.component.scss']
 })
 export class CatalogueComponent {
-  icon=faChevronRight
-  icon2=faChevronLeft
+
+    // font awesome icons
+  icon=faChevronRight;
+  icon2=faChevronLeft;
+  magnifyGlass=faMagnifyingGlass;
+
+  // variables and array
   p:number=1;
   blist:bici[]=[];
+  searchOn:boolean=false;
+  searchlist:bici[]=[]
   constructor(private http:HttpClient){}
   ngOnInit(){
   this.http.get<any>('https://localhost:7284/ProductModels').subscribe((result)=>{
@@ -34,7 +42,20 @@ decreasePage(p:number){
 this.p--;
 }
 
+serchArticles(input :HTMLInputElement){
+  if(input.value == ' '){
+    this.searchOn = false
+  }else{
+
+    this.searchOn = true
+     var lista =  this.blist.filter(elem=>elem.name.includes(input.value))
+     this.searchlist = lista
+     console.log(this.searchlist)
+  }
+
 }
+}
+
 
 interface bici{
   productModelId:number
