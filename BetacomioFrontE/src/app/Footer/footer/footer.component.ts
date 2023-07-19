ebcfce32e7d33851c34006b88150d1548a54b770
ChristianvVett <chrisvett8502@gmail.com>
2023-07-19 17:17:49 +0200

    Card login e signup

--- BetacomioFrontE/src/app/Footer/footer/footer.component.ts
+++ BetacomioFrontE/src/app/Footer/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {faFacebook} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+ icon=faFacebook;
 }
