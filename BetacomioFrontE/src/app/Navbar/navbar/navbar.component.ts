c0827b50c0ea32b6e68a7579958cc9b68f86fb72
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-09-05 17:01:26 +0200

    push it to the limit

--- BetacomioFrontE/src/app/Navbar/navbar/navbar.component.ts
+++ BetacomioFrontE/src/app/Navbar/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component , OnInit , Renderer2 , OnChanges, SimpleChanges,ChangeDetectorRef,AfterViewChecked  } from '@angular/core';
+import { Component ,ChangeDetectorRef  } from '@angular/core';
 import {faBars} from '@fortawesome/free-solid-svg-icons'
 import {faCartShopping} from '@fortawesome/free-solid-svg-icons';
 import { TokenService } from 'src/app/Services/token.service';
@@ -9,7 +9,7 @@ declare let paypal: any;
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements AfterViewChecked {
+export class NavbarComponent {
   icon=faBars;
   cart=faCartShopping;
   rew: string = sessionStorage.getItem('dati');
@@ -20,55 +20,11 @@ export class NavbarComponent implements AfterViewChecked {
   session: string;
   cfd:boolean;
   showMenu:boolean;
-  addScript:boolean = false;
-  finalAmount: number = 950;
   tok = this.Token.token();
   constructor(private Token:TokenService,private cdr: ChangeDetectorRef){
 
   }
-  paypalconfig = {
-    env: 'sandbox',
-    client: {
-      sandbox: 'ARWPs-atx9sIQ__3FrCAfobBvHTATMBU3u6tX2dbNpdqVtRP6GoAZZr1eAL1BQmWrGiGA80POdkljZF0',
-    },
-    commit: true,
-    payment: (data, actions) => {
-      return actions.payment.create({
-        payment: {
-          transactions: [
-            {
-              amount: { total: this.finalAmount, currency: 'EUR' }
-            }
-          ]
-        }
-      });
-    },
-    onAuthorize: (data, actions) => {
-      return actions.payment.execute().then((payment) => {
 
-      })
-    }
-  };
-  addPaypalScript(){
-    this.addScript = true;
-    return new Promise((resolve,reject)=>{
-      let scripttagElement = document.createElement('script');
-      scripttagElement.src='https://www.paypalobjects.com/api/checkout.js';
-      scripttagElement.onload=resolve;
-      const container = document.getElementById("sss");
-      document.body.appendChild(scripttagElement)
-    })
-  }
-  ngAfterViewChecked(): void {
-    if (!this.addScript) {
-      window.onload=()=>{
-
-        this.addPaypalScript().then(() => {
-          paypal.Button.render(this.paypalconfig,'#paycheck')
-        });
-      }
-    }
-  }
   ngOnInit() {
     this.Token.isAuthenticated$.subscribe(isAuthenticated => {
       this.IsLogged = isAuthenticated;
--- BetacomioFrontE/src/app/Payment/paypal/paypal.component.ts
+++ BetacomioFrontE/src/app/Payment/paypal/paypal.component.ts
@@ -8,43 +8,47 @@ declare let paypal:any
 export class PaypalComponent implements AfterViewChecked {
   addScript:boolean = false;
   finalAmount: number = 1;
-  paypalconfig={
-    env:'sandbox',
-    client:{
-      // sandbox:'ARWPs-atx9sIQ__3FrCAfobBvHTATMBU3u6tX2dbNpdqVtRP6GoAZZr1eAL1BQmWrGiGA80POdkljZF0',
+  paypalconfig = {
+    env: 'sandbox',
+    client: {
+      sandbox: 'ARWPs-atx9sIQ__3FrCAfobBvHTATMBU3u6tX2dbNpdqVtRP6GoAZZr1eAL1BQmWrGiGA80POdkljZF0',
     },
-    commit:true,
-    payment:(data,actions)=>{
+    commit: true,
+    payment: (data, actions) => {
       return actions.payment.create({
-        payment:{
-          transactions:[
+        payment: {
+          transactions: [
             {
-              amount:{total:this.finalAmount,currency:'EUR'}
+              amount: { total: this.finalAmount, currency: 'EUR' }
             }
           ]
         }
       });
     },
-    onAuthorize:(data, actions)=>{
-      return actions.payment.execute().then((payment)=>{
+    onAuthorize: (data, actions) => {
+      return actions.payment.execute().then((payment) => {
 
       })
-  }
+    }
   };
-  ngAfterViewChecked(): void {
-      if(!this.addScript){
-        this.addPaypalScript().then(()=>{
-          paypal.Button.render(this.paypalconfig,'#paycheck')
-        })
-      }
-  }
   addPaypalScript(){
     this.addScript = true;
     return new Promise((resolve,reject)=>{
       let scripttagElement = document.createElement('script');
       scripttagElement.src='https://www.paypalobjects.com/api/checkout.js';
       scripttagElement.onload=resolve;
+      const container = document.getElementById("sss");
       document.body.appendChild(scripttagElement)
     })
+  }
+  ngAfterViewChecked(): void {
+    if (!this.addScript) {
+
+
+        this.addPaypalScript().then(() => {
+          paypal.Button.render(this.paypalconfig,'#paycheck')
+        });
+
+    }
   }
 }
