397e976d2e1fc284d32a00a3805e7a0d51bab2a3
Giuseppe-Buscemi95 <giuseppe.b95@outlook.it>
2023-09-16 16:10:00 +0200

    ciao

--- BetacomioFrontE/src/app/Contacts/contacts/contacts.component.ts
+++ BetacomioFrontE/src/app/Contacts/contacts/contacts.component.ts
@@ -14,6 +14,7 @@ import { Router } from '@angular/router';
 export class ContactsComponent {
   constructor(private emailsender:EmailsenderService, http:HttpClient,private router: Router){
     this.http = http
+    //ciao
   }
   okFile: boolean = false;
   ciao:Uint8Array[] = [];
